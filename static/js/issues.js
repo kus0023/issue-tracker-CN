a881b57a18def04a7845481af4b3ea4e2212935b
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2023-08-12 10:02:08 +0530

    searching is completed.

--- static/js/issues.js
+++ static/js/issues.js
@@ -3,28 +3,32 @@ document.addEventListener('DOMContentLoaded', function () {
 
     initializeLabelSelect();
 
-    formSubmit();
+    // formSubmit();
 
     initializeAddFloatButton();
 });
 
+
+function formSubmit() {
+
+    const formElem = document.getElementById('issue-search-form');
+
+    formElem.addEventListener('submit', function(e){
+
+        e.preventDefault();
+
+        let formData = new FormData(formElem);
+
+        console.log(formData.entries.toString());
+    })
+
+
+}
+
 function initializeAuthorSelect() {
 
     let elem = document.getElementById('author-select');
-    // elem.innerHTML = '';
-
-    // let authors = ['Kus', 'John', 'Marry'];
-
-    // authors.forEach(author => {
-
-    //     let optionElem = document.createElement('option');
-    //     optionElem.innerHTML = author;
-    //     optionElem.value = author;
-
-    //     elem.append(optionElem);
-    // });
-
-
+    
     const options = {};
     let instances = M.FormSelect.init(elem, options);
 
@@ -33,42 +37,9 @@ function initializeAuthorSelect() {
 function initializeLabelSelect() {
 
     let elem = document.getElementById('label-select');
-    // elem.innerHTML = '';
-
-    // let labels = ['Bigginer', 'Bug', 'enhancement'];
-
-    // labels.forEach(label => {
-
-    //     let optionElem = document.createElement('option');
-    //     optionElem.innerHTML = label;
-    //     optionElem.value = label;
-
-    //     elem.append(optionElem);
-    // });
-
+    
     const options = {};
     let instances = M.FormSelect.init(elem, options);
-}
-
-function formSubmit() {
-
-    // const formElem = document.getElementById('issue-search-form');
-
-    // formElem.addEventListener('submit', function(e){
-
-    //     e.preventDefault();
-
-    //     let formData = new FormData(formElem);
-
-    //     console.log(formData);
-
-    //     fetch('/abara-ka-dabra', {
-    //         method: 'POST',
-    //         body: formData
-    //     })
-    // })
-
-
 }
 
 function initializeAddFloatButton() {
